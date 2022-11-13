88d3f248fcbdac067add760687b8c3908f9d6e5a
Aza#ZLO <MaloyMeee@yandex.ru>
2022-11-13 23:32:31 +0300

    Календарь

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
+package SmartMirror;
 public class Main {
     public static void main(String[] args) {
         WindowApp app = new WindowApp(); //Создаем экземпляр нашего приложения
-
         app.setVisible(true); //Отображаем наше окно
         app.fraze();
         app.clock();
-
-
+        app.Calendar();
+        app.data();
     }
 }
