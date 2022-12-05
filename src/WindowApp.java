70c4f1952c4c1cb55b565d22d7fc97b73068d881
Aza#ZLO <MaloyMeee@yandex.ru>
2022-12-06 02:00:15 +0300

    debug compiling

--- src/WindowApp.java
+++ src/WindowApp.java
@@ -194,7 +194,7 @@ public class WindowApp extends JFrame { //Наследуя от JFrame мы по
                         String A="<html>";
                         String c="&nbsp ";
                         String c2="&nbsp &nbsp ";
-                        String c3="&nbsp\u2009 \u2009";
+                        String c3="&nbsp \u2009  \u2009";
                         String c4=" &nbsp &nbsp";
                         String c5=" &nbsp \u2009\u2009 ";
                         String c6="&nbsp \u2009 ";
