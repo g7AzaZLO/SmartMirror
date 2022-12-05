9b0c0947343892883345ad8953a9d9b20dfdc314
Aza#ZLO <MaloyMeee@yandex.ru>
2022-12-06 02:19:09 +0300

    debug compiling

--- src/WindowApp.java
+++ src/WindowApp.java
@@ -223,7 +223,7 @@ public class WindowApp extends JFrame { //Наследуя от JFrame мы по
                         String[] weekdayNames = new DateFormatSymbols().getShortWeekdays();
                         do
                         {
-                            A+=weekdayNames[weekday];A+=c;A+=" ";A+=" ";
+                            A+=weekdayNames[weekday];A+=c;A+="&nbsp";
                             d.add(Calendar.DAY_OF_MONTH, 1);
                             weekday = d.get(Calendar.DAY_OF_WEEK);
                         }
